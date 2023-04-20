559066f4d92da0b6ade8697637eaf294699f0e9f
wangzc <leavesformsn@hotmail.com>
2023-04-20 17:05:13 +0800

    DDD + spring boot 3 + spring data jdbc

--- eleven-learning/src/main/java/com/wzc/learning/BitOperate.java
+++ eleven-learning/src/main/java/com/wzc/learning/BitOperate.java
@@ -9,11 +9,20 @@ public class BitOperate {
         System.out.println();
     }
 
+    public static void printDecimal(int num) {
+        System.out.println(num);
+    }
+
+    // 在位运算中，1 是一个很有意义的数字，一般都是通过对 1 进行左移，右移，然后 &  | ^ 来实现一些指定位操作。
+    //  00000000000000011000101101100110
+    //& 11111111111111111111111111111011
+    //  --------------------------------
+    //  00000000000000011000101101100010
     public static void main(String[] args) {
-        int n = 1;
-        printBinary(n);
-        n = -~n;
-        printBinary(n);
+        int x = -101221;
+        printBinary(x);
+        printBinary((1 << 30));
+        System.out.println((x & (1 << 30))>0);
     }
 
 }
